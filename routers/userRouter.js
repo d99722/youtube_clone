b4ff000bd0dbfa0c9eb918fb081c8a8c4de45321
d99722 <d99722@gmail.com>
2020-09-26 02:19:07 +0900

    day 10

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,17 +1,16 @@
 import express from "express";
+import routes from "../routes";
 import {
   users,
-  changePassword,
-  editProfile,
   userDetail,
+  editProfile,
+  changePassword,
 } from "../controllers/userController";
-import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,19 +1,19 @@
 import express from "express";
-import { editProfile } from "../controllers/userController";
-import {
-  deleteVideo,
-  editVideo,
-  upload,
-  videoDetail,
-  videos,
-} from "../controllers/videoController";
 import routes from "../routes";
+import {
+  getUpload,
+  postUpload,
+  videoDetail,
+  editVideo,
+  deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
